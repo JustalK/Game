b14f3efbcb1aeef19b4da525b4702d3ca629bef6
Latsuj <justal.kevin@gmail.com>
2015-11-09 23:48:15 +0100

    [3] Adding the game over and the restart menu

--- Game/www/js/main.js
+++ Game/www/js/main.js
@@ -1,17 +1,39 @@
 $(function(){
-	  var way="up";
-	  $( ".app" ).bind( "vmousedown", tapHandler );
+	  var up=true;
+	  var lose=false;
+	  $(".app").bind( "vmousedown", tapHandler );
+	  $("#lose").bind( "vmousedown", retry );
 	  var windowHeight = $(window).height();
 	  
 	  function tapHandler( event ){
-		  if(way=="up") {
-				way="down";
-				$("#cube").attr("src","img/1.png");
-				$("#cube").stop().animate({"bottom":windowHeight-50+"px"},800);
-		  } else {
-			  	way="up";
-				$("#cube").attr("src","img/2.png");
-				$("#cube").stop().animate({"bottom":"0"},800);
+		  if(!lose) {
+			  if(up) {
+					up=false;
+					$("#cube").attr("src","img/1.png");
+					$("#cube").stop().animate({"bottom":windowHeight-50-35+"px"},700,function() {
+						gameOver();
+					});
+			  } else {
+				  	up=true;
+					$("#cube").attr("src","img/2.png");
+					$("#cube").stop().animate({"bottom":"35px"},700,function() {
+						gameOver();
+					});
+			  }
 		  }
+	  }
+	  
+	  function retry(event) {
+		  if(lose) {
+			  var up=true;
+			  lose=false;		
+			  $("#lose").css("opacity","0");  
+			  $("#cube").css("bottom","50%");
+		  }
+	  }
+	  
+	  function gameOver() {
+		  lose=true;
+		  $("#lose").css("opacity","1");
 	  }
 });
